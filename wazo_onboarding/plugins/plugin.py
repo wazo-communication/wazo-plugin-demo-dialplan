9a06c79846404ff22a933d30f1a8f7abb2cdcd1a
Akshita Patel <akshita.patel@wazo.io>
2024-06-25 16:02:06 -0400

    Revert "Merge pull request #2 from AkshitaPatel/New-Plugin"
    This reverts commit 41199f8bcd348b5056fbba105fdee2c4af44ed20, reversing
    changes made to f40f87092484e8a673bb8914f5af204236f9b44f.

--- wazo_onboarding/plugins/plugin.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-logger = logging.getLogger(__name__)
-
-
-class Plugin:
-    def load(self, dependencies):
-        logger.info("This is my new Plugin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
